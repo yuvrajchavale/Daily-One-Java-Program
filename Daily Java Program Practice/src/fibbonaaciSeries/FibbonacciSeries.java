dc45ace63a65c4f4ed4fa964dd90c9ae55720366
yuvrajchavale <chawaleyuvraj@gmail.com>
2024-08-06 17:33:30 +0530

    fibonacci

---
+++ Daily Java Program Practice/src/fibbonaaciSeries/FibbonacciSeries.java
@@ -0,0 +1,28 @@
+package fibbonaaciSeries;
+
+public class FibbonacciSeries {
+
+	public static void main(String[] args) {
+		int num = 10; // still ten
+		seriesPrint(num);
+
+	}
+
+    public static void seriesPrint(int num) {
+    	int a = 0;
+    	int b = 1;
+    	
+    	
+    	for (int i = 1; i <=num; i++) {
+		
+    	System.out.println(a + " ");
+    		
+    	int nextNum = a + b;
+    	a =b ;
+    	b = nextNum;
+    	
+    	
+    	}
+    	
+    }	
+}
