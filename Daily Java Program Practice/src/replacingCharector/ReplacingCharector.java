da2366200cb17cf61464c634ab599e43c9742d34
yuvrajchavale <chawaleyuvraj@gmail.com>
2024-09-04 16:12:26 +0530

    Specific word replace

---
+++ Daily Java Program Practice/src/replacingCharector/ReplacingCharector.java
@@ -0,0 +1,11 @@
+package replacingCharector;
+
+public class ReplacingCharector {
+
+	public static void main(String[] args) {
+		String myStr = "Program" ;
+		System.out.println(myStr.replace('g', 'p'));
+
+	}
+
+}
