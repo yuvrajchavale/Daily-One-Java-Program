70e391e8baa5593a212effebfa8f1ffcd45461e7
yuvrajchavale <chawaleyuvraj@gmail.com>
2024-09-05 21:49:35 +0530

    Practice

---
+++ Daily Java Program Practice/src/fibbonaci/FibbonaciSeries.java
@@ -0,0 +1,20 @@
+package fibbonaci;
+
+public class FibbonaciSeries {
+
+	public static void main(String[] args) {
+		int n= 10 ; 
+		 int n1 = 0;
+		 int n2 = 1 ;
+		 for(int i = 0 ; i < n ; i++) {
+			 System.out.println(n1);
+			 
+			 //swap logic
+			  int n3 = n1 + n2 ;
+			  n1 = n2 ;
+			  n2 = n3 ;
+		 }
+
+	}
+
+}
