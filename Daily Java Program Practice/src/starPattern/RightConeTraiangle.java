f49b1cd12810bb0add78651dcb472796213db5df
yuvrajchavale <chawaleyuvraj@gmail.com>
2024-09-14 00:05:12 +0530

    star pattern

---
+++ Daily Java Program Practice/src/starPattern/RightConeTraiangle.java
@@ -0,0 +1,25 @@
+package starPattern;
+
+public class RightConeTraiangle {
+
+	public static void main(String[] args) {
+		for(int i=1 ; i<=4 ; i++) 
+		{
+			for(int j=1 ; j <= i ; j++) 
+			{
+				System.out.print("*");
+			}
+			
+		       System.out.println();
+		}
+		
+		for(int i = 1 ; i<=4 ; i++) {
+			for(int j = 3 ; j>=i ; j--) {
+				System.out.print("*");
+			}
+		System.out.println();
+		}
+
+	}
+
+}
